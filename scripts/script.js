1d0d22276b2f8deae5f558f730ac95429797f856
Nikita Ivochkin <invginaku@yandex.com>
2021-02-08 15:57:08 +0300

    fixed profile div and script

--- scripts/script.js
+++ scripts/script.js
@@ -7,23 +7,24 @@ let formElement = document.querySelector('.popup__form');
 let nameInput = document.querySelector('.popup__input_type_name');
 let jobInput = document.querySelector('.popup__input_type_status');
 
-editButton.addEventListener('click',
 function popupOpened () {
     popup.classList.add('popup_opened');
     nameInput.value = profileName.textContent
     jobInput.value = profileStatus.textContent
 }
-);
-closeButton.addEventListener('click',
+
 function popupClosed () {
     popup.classList.remove('popup_opened');
 }
-);
 
 function handleFormSubmit (evt) {
     evt.preventDefault();
     profileName.textContent = nameInput.value;
     profileStatus.textContent = jobInput.value;
-    popup.classList.remove('popup_opened');
+    popupClosed ();
 }
+editButton.addEventListener('click', popupOpened);
+
+closeButton.addEventListener('click', popupClosed);
+
 formElement.addEventListener('submit', handleFormSubmit);
